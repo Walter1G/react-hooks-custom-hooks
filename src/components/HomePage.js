f2d60f4ce29ca7fc6e10db7f742f9dfd9f48eb1d
Walter N <2760walter@gmail.com>
2023-11-04 21:35:33 +0300

    completed assignment

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,15 +12,15 @@ function HomePage() {
 
   const { data: posts, isLoaded } = useQuery("http://localhost:4000/posts")
 
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setIsLoaded(false);
+  //   fetch("http://localhost:4000/posts")
+  //     .then((r) => r.json())
+  //     .then((posts) => {
+  //       setPosts(posts);
+  //       setIsLoaded(true);
+  //     });
+  // }, []);
 
   // set the document title
   // useEffect(() => {
